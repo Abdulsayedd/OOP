57c46e25892001f97d1153bc3ba1086d6bf32579
Mohammed Abdulsayed <93003919+Abdulsayedd@users.noreply.github.com>
2022-01-14 21:17:37 +0200

    C# Program Calculates the average and sum 100 num

--- OOP_2020 Q2-b.cs
+++ OOP_2020 Q2-b.cs
@@ -3,23 +3,39 @@ using System.Collections.Generic;
 using System.Text;
 namespace program
 {
+    public class average
+    {
+        double sum = 0;
+        public void avg(int[] arr)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            { sum += arr[i]; }
+            sum /= 100;
+            Console.WriteLine("Average = "+sum);
+        }
+        public void maxx(int[] arr)
+        {
+            int x = 0;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(x<arr[i])
+                {
+                    x = arr[i];
+                }
+            }
+            Console.WriteLine("Max = "+x);
+        }
+    }
     public class Program
-    { 
+    {
         static void Main(string[] Args)
         {
             int[] arr = new int[100];
-            double sum = 0;
-            int maxx = 0;
             for(int i = 0; i < 100; i++)
-            {
-                arr[i] = int.Parse(Console.ReadLine());
-                sum += arr[i];
-                if(arr[i]>maxx)
-                    maxx=arr[i];
-            }
-            sum /= 100;
-            Console.WriteLine("Average = {0} , Max = {1}",sum,maxx);
-
+            { arr[i] = int.Parse(Console.ReadLine()); }
+            average a = new average();
+            a.avg(arr);
+            a.maxx(arr);
         }
     }
 }
