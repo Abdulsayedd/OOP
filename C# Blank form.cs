b5782c62a9f61f6bd8fd8198aeebfdab09207e32
Mohammed Abdulsayed <93003919+Abdulsayedd@users.noreply.github.com>
2022-01-14 09:22:47 +0200

    C# Blank form

---
+++ C# Blank form.cs
@@ -0,0 +1,13 @@
+namespace WinFormsApp
+{
+    partial class Form1
+    {
+      private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
+        }
+    }
+}
