26622ba8eed27a34a79d8ef410195a9868eea639
Mohammed Abdulsayed <93003919+Abdulsayedd@users.noreply.github.com>
2022-01-15 15:32:27 +0200

    C# Program "DATABASE"

---
+++ OOP_2021 Q2-a.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace program
+{
+    public class Program
+    {
+        static void Main(string[] Args)
+        {
+            string s1 = "database";
+            Console.WriteLine(s1.ToUpper()) ;
+        }
+    }
+}
